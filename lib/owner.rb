62e681850a8d536dbe5b9ca0577d99552238b9e0
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 03:15:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+  attr_accessor
+  @@all = []
+  @@count = 0 
+  
+  def initialize(owner)
+    @owner = owner
+    @@all << owner
+    @@count += 1 
+  end
+  
+  def count
+    @@count 
+  end
 end
