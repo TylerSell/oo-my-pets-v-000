2ad0b8166ce8bf4d3e7f4849a2a6fe90006e6dc6
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 03:46:50 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,13 @@
 class Cat
+  attr_writer :mood
+  
   def initialize(name, mood = "nervous")
     @name = name.freeze 
     @mood = mood 
   end
   
-  def self.mood=(mood) 
-    @mood = mood
+  def self.mood 
+    @mood
   end
   
   def name
