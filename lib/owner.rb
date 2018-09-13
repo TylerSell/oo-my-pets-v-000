3803754a366e3e7b9fa9debceac814505e7fdce5
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 04:09:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,11 +59,15 @@ class Owner
   end
   
   def play_with_cats 
-    
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
   
   def feed_fish
-    
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
   
   def sell_pets 
