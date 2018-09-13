09537fcf412ed1953def8b253ea79ac40c55ff0f
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 03:48:24 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
 class Fish
-  # code goes here
+  attr_writer :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name.freeze 
+    @mood = mood 
+  end
+  
+  def mood 
+    @mood
+  end
+  
+  def name
+    @name 
+  end
 end
