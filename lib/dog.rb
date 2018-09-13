737cea6b651138e6bba16a6f47e24777af94d2a6
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 03:48:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-  # code goes here
+  attr_writer :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name.freeze 
+    @mood = mood 
+  end
+  
+  def mood 
+    @mood
+  end
+  
+  def name
+    @name 
+  end
 end
